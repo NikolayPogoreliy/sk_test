24b1546c4221a084bfd4b414d7194043bea29b19
mykola <nikolay.pogoreliy@gmail.com>
2021-03-22 21:49:28 +0200

    generic args

--- backend/vms/schema/queries.py
+++ backend/vms/schema/queries.py
@@ -4,13 +4,38 @@ from requests.auth import HTTPBasicAuth
 
 
 class AccountsQuery:
-    args = generic.GenericScalar()
+    vmsaccounts = generic.GenericScalar(
+        # generic.GenericScalar,
+        # start=graphene.Int(required=True, default_value=0),
+        # number=graphene.Int(required=True, default_value=10),
+        # order_predicate=graphene.String(required=False),
+        # order_direction=graphene.String(required=False),
+        # query=graphene.String(required=False),
+        # user_id=graphene.String(required=False)
+    )
 
-    def resolve_args(self, info):
+    def resolve_vmsaccounts(
+            self, info,
+            # start=0,
+            # number=10,
+            # order_predicate=None,
+            # order_direction=None,
+            # query=None,
+            # user_id=None
+    ):
+        data = dict(start=0, number=10)
+        # if order_predicate:
+        #     data['oder_predicate'] = order_predicate
+        # if order_direction:
+        #     data['oder_direction'] = order_direction
+        # if query:
+        #     data['query'] = query
+        # if user_id:
+        #     data['user_id'] = user_id
         response = requests.post(
-            url="https://staging.stellen-anzeiger.ch/admin/api/vacancy/search",
+            url="https://staging.stellen-anzeiger.ch/admin/api/account/search",
             auth=HTTPBasicAuth('analytics', 'analytics'),
-            json={'start': 0, 'number': 1}
+            json=data
         ).json()
         return response.get('data', [])
         # return {"adr": "awe"}
