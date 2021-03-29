4951d3f08fd7d4ddd30497da29db18ab611816c8
mykola <nikolay.pogoreliy@gmail.com>
2021-03-29 17:51:18 +0300

    query report docstrings

--- backend/report/schema/queries.py
+++ backend/report/schema/queries.py
@@ -7,28 +7,12 @@ from backend.report.schema.types import ReportType
 
 
 class ReportQuery:
+    """ Get list of reports or single report"""
     reports = DjangoFilterConnectionField(ReportType)
     report = graphene.Field(ReportType, id=graphene.String())
 
-    # def resolve_reports(self, info, **kwargs):
-    #     return Report.objects.all()
-    #     if 'limit' in kwargs and 'page' in kwargs:
-    #         limit = int(kwargs.get('limit', 15))
-    #         if limit <= 0:
-    #             limit = 15
-    #         total_pages = int(ceil(qs.count() / limit))
-    #         page = int(kwargs.get('page', 1))
-    #         if page <= 0:
-    #             page = 1
-    #         if page > total_pages:
-    #             page = total_pages
-    #
-    #         from_row = limit * (page - 1)
-    #         to_row = from_row + limit + 1
-    #         qs = qs[from_row: to_row]
-    #     return qs
-
     def resolve_report(self, info, id):
+        """ Retrieve certain report """
         report = Report.objects.filter(id=from_global_id(id)[1])
         if report:
             return report.first()
